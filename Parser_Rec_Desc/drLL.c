8b638afe734e40b721811dc83a5d1b2c028bbb55
r-v1adi <100508621@alumnos.uc3m.es>
2026-03-09 13:09:41 +0100

    Parser 1.0

--- Parser_Rec_Desc/drLL.c
+++ Parser_Rec_Desc/drLL.c
@@ -10,8 +10,8 @@
 #define T_OPERATOR 1002
 #define T_VARIABLE 1003
 
-void ParseYourGrammar(); /// Dummy Parser
-void ParseAxiom();       /// Prototype for forward reference
+void ParseYourGrammar(); // Dummy Parser
+void ParseAxiom();       // Prototype for forward reference
 
 struct s_tokens
 {
@@ -19,8 +19,8 @@ struct s_tokens
     int old_token;         // Sometimes we need to check the previous token
     int number;            // The value of the number
     int old_number;        // old number value
-    char variable_name[8]; /// variable name
-    char old_var_name[8];  /// old variable name
+    char variable_name[8]; // variable name
+    char old_var_name[8];  // old variable name
     int token_val;         // the arithmetic operator
     int old_token_val;     // old arithmetic operator
 };
@@ -30,21 +30,21 @@ struct s_tokens tokens = {0, 0, 0, -1, "", "", 0, -1}; // contains initial value
 int line_counter = 1;
 
 void update_old_token()
-{ /// Sometimes we need to check the previous token
+{ // Sometimes we need to check the previous token
     tokens.old_token = tokens.token;
     tokens.old_number = tokens.number;
-    strcpy(tokens.old_var_name, tokens.variable_name); /// Copy variable names
+    strcpy(tokens.old_var_name, tokens.variable_name); // Copy variable names
     tokens.old_token_val = tokens.token_val;
 }
 
 void init_tokens()
-{ ///  Not really neccesary
+{ //  Not really neccesary
     tokens.token = 0;
     tokens.old_token = 0;
     tokens.number = 0;
     tokens.old_number = -1;
-    strcpy(tokens.old_var_name, "");  /// erase old variable name
-    strcpy(tokens.variable_name, ""); /// Erase variable name
+    strcpy(tokens.old_var_name, "");  // erase old variable name
+    strcpy(tokens.variable_name, ""); // Erase variable name
     tokens.token_val = 0;
     tokens.old_token_val = -1;
 }
@@ -52,7 +52,7 @@ void init_tokens()
 int rd_lex()
 {
 
-    /// DO NOT MODIFY THE CODE INSIDE THE YYLEX FUNCTION WITHOUT PERMISSION !!!
+    // DO NOT MODIFY THE CODE INSIDE THE YYLEX FUNCTION WITHOUT PERMISSION !!!
 
     int c;
     int cc;
@@ -62,11 +62,11 @@ int rd_lex()
         c = getchar();
         if (c == '\n')
             line_counter++; // info for rd_syntax_error()
-    } while (c == '\t' || c == ' ' || c == '\r'); /// \r is part of a newline in some Operating Systems
+    } while (c == '\t' || c == ' ' || c == '\r'); // \r is part of a newline in some Operating Systems
 
     if (isdigit(c))
-    {                     /// Token Number is [Digit]+
-        ungetc(c, stdin); /// This returns one character to the standard input stream
+    {                     // Token Number is [Digit]+
+        ungetc(c, stdin); // This returns one character to the standard input stream
         update_old_token();
         scanf("%d", &tokens.number);
         tokens.token = T_NUMBER;
@@ -74,24 +74,24 @@ int rd_lex()
     }
 
     if (isalpha(c))
-    { /// Token Variable of type Letter[Digit|Letter]?
+    { // Token Variable of type Letter[Digit|Letter]?
         update_old_token();
         cc = getchar();
         if (isdigit(cc) || isalpha(cc))
         {
-            sprintf(tokens.variable_name, "%c%c", c, cc); /// This copies the Letter.Digit|Letter name in the variable name
+            sprintf(tokens.variable_name, "%c%c", c, cc); // This copies the Letter.Digit|Letter name in the variable name
         }
         else
         {
             ungetc(cc, stdin);
-            sprintf(tokens.variable_name, "%c", c); /// This copies the single Letter name in the variable name
+            sprintf(tokens.variable_name, "%c", c); // This copies the single Letter name in the variable name
         }
         tokens.token = T_VARIABLE;
         return (tokens.token); // returns the Token for Variable
     }
 
     if (c == '+' || c == '-' || c == '*' || c == '/')
-    { /// Remember that OTHER SYNBOLS ARE returned as literals
+    { // Remember that OTHER SYNBOLS ARE returned as literals
         update_old_token();
         tokens.token_val = c;
         tokens.token = T_OPERATOR;
@@ -99,7 +99,7 @@ int rd_lex()
     }
 
     if (c == EOF)
-    { /// End Of Archive detection for enhanced Batch Processing
+    { // End Of Archive detection for enhanced Batch Processing
         exit(0);
     }
 
@@ -125,27 +125,106 @@ void MatchSymbol(int expected_token)
     }
     else
     {
-        rd_lex(); /// read next Token
+        rd_lex(); // read next Token
     }
 }
 
-// #define ParseLParen() 	MatchSymbol ('(') ; // More concise and efficient definitions
-// #define ParseRParen() 	MatchSymbol (')') ; ///   rather than using functions
-/// The actual recomendation is to use MatchSymbol in the code rather than theese macros
+// #define ParseLParen() MatchSymbol('('); // More concise and efficient definitions
+// #define ParseRParen() MatchSymbol(')'); //   rather than using functions
+// The actual recomendation is to use MatchSymbol in the code rather than theese macros
 
-void ParseYourGrammar()
+// Expresión -> ( Resto ) | Número | Variable
+// Resto -> Operador Expresión Expresión | = Variable Expresión Condición | ? Expresión Expresión Expresión
+// Condición -> Lambda | Expresión Expresión
+// Operador -> + | - | * | /
+// Número -> 0..9
+// Variable -> a..z
+
+void ParseNumero() // Número -> 0..9
 {
+    MatchSymbol(T_NUMBER);
 }
 
-void ParseAxiom()
-{                       /// Axiom ::= \n
-    ParseYourGrammar(); /// Dummy Parser. Complete this with your design
-    if (tokens.token == '\n')
+void ParseVariable() // Variable -> a..z
+{
+    MatchSymbol(T_VARIABLE);
+}
+
+void ParseOperador() // Operador -> + | - | * | /
+{
+    MatchSymbol(T_OPERATOR);
+}
+
+void ParseCondicion() // Condición -> Lambda | Expresión Expresión
+{
+    // Condición -> Lambda
+    if (tokens.token == ')') // Se verifica el conjunto Siguiente(Condición)
     {
-        printf("\n");
-        MatchSymbol('\n');
+        return tokens.token;
     }
-    else
+    // Condición -> Expresión Expresión
+    else if (tokens.token == T_VARIABLE || tokens.token == '(' || tokens.token == T_NUMBER) // Verificamos el conjunto Primero(Expresion)
+    {
+        ParseAxiom();
+        ParseAxiom();
+    }
+    else // ERROR
+    {
+        rd_syntax_error(-1, tokens.token, "-- Unexpected Token (Expected:%d=None, Read:%d) at end of Parsing\n");
+    }
+}
+
+void ParseResto() // Resto -> Operador Expresión Expresión | = Variable Expresión Condición | ? Expresión Expresión Expresión
+{
+    // Resto -> Operador Expresión Expresión
+    if (tokens.token == T_OPERATOR)
+    {
+        ParseOperador();
+        ParseAxiom();
+        ParseAxiom();
+    }
+    // Resto -> = Variable Expresión Condición
+    else if (tokens.token == '=')
+    {
+        MatchSymbol('=');
+        ParseVariable();
+        ParseAxiom();
+        ParseCondicion();
+    }
+    // Resto -> ? Expresión Expresión Expresión
+    else if (tokens.token == '?')
+    {
+        MatchSymbol('?');
+        ParseAxiom();
+        ParseAxiom();
+        ParseAxiom();
+    }
+    else // ERROR
+    {
+        rd_syntax_error(-1, tokens.token, "-- Unexpected Token (Expected:%d=None, Read:%d) at end of Parsing\n");
+    }
+}
+
+void ParseAxiom() // Expresión -> ( Resto ) | Número | Variable
+{
+    // Expresión -> ( Resto )
+    if (tokens.token == '(')
+    {
+        MatchSymbol('(');
+        ParseResto();
+        MatchSymbol(')');
+    }
+    // Expresión -> Número
+    else if (tokens.token == T_NUMBER)
+    {
+        ParseNumero();
+    }
+    // Expresión -> Variable
+    else if (tokens.token == T_VARIABLE)
+    {
+        ParseVariable();
+    }
+    else // ERROR
     {
         rd_syntax_error(-1, tokens.token, "-- Unexpected Token (Expected:%d=None, Read:%d) at end of Parsing\n");
     }
@@ -156,7 +235,7 @@ int main(int argc, char **argv)
     // Usage :  drLL     ==> evalute multiple Input Lines until some error appears   NORMAL USAGE
     //--        drLL -s  ==> evaluate a single Input Line                            USE ONLY IN CASE your program FREEZES
     //
-    /// DO NOT MODIFY THE CODE INSIDE THE MAIN FUNCTION WITHOUT PERMISSION !!!
+    // DO NOT MODIFY THE CODE INSIDE THE MAIN FUNCTION WITHOUT PERMISSION !!!
 
     int flagMultiple = 1;
 
@@ -168,7 +247,7 @@ int main(int argc, char **argv)
         }
     }
 
-    rd_lex(); /// Read first Token only once
+    rd_lex(); // Read first Token only once
     do
     {
         ParseAxiom();
